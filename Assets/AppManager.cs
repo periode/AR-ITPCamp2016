c61061848e66ee24f0d09b996489d009ef1c0b8a
Pierre Depaz <pierre.depaz@gmail.com>
2016-06-16 15:39:43 -0400

    added unity project

---
+++ Assets/AppManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AppManager : MonoBehaviour {
+
+	public bool isTracked = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isTracked)
+			Debug.Log ("we know if something is being tracked!");
+	}
+}
